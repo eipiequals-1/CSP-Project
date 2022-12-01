e4e3cb4b7b4c9df403157ee05b3420c25fbedca2
eipiequals-1 <80728993+eipiequals-1@users.noreply.github.com>
2022-12-01 09:49:33 -0800

    Added php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>PHP Application</title>
+    </head>
+<body>
+    <?php
+    $color = "red";
+    echo '<h1>' . $color . '</h1>'; 
+    
+    ?> 
+</body>
+</html>
