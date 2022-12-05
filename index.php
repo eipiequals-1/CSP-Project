7ae368922feb2bfcb3977969e4aac1d9d83b07dd
eipiequals-1 <80728993+eipiequals-1@users.noreply.github.com>
2022-12-05 09:23:30 -0800

    modified some php and css

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<html>
-    <head>
-        <title>PHP Application</title>
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>French Recipes</title>
+
+    <link rel="stylesheet" href="index.css">
+</head>
 <body>
+    <nav>
+        <a>Hello World</a>
+    </nav>
+
     <?php
     $color = "red";
     echo '<h1>' . $color . '</h1>'; 
-    
     ?> 
 </body>
 </html>
