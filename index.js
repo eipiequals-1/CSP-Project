7b70e1ed37aebf11546996c0b08e77a5c74d166b
eipiequals-1 <80728993+eipiequals-1@users.noreply.github.com>
2022-12-08 10:04:39 -0800

    update slides

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-function aspectRatio() {
-    
+// code
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+  const ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+  return { width: srcWidth * ratio, height: srcHeight * ratio };
 }
 
-window.addEventListener("DOMContentLoaded", _ => {
-    
+function resize() {
+  const mainImages = document.getElementsByClassName("main-recipe-img");
+  for (let image of mainImages) {
+    const resize = calculateAspectRatioFit(
+      image.width,
+      image.height,
+      window.innerWidth / 2.0,
+      300
+    );
+    image.style.width = resize.width + "px";
+    image.style.height = resize.height + "px";
+  }
+}
 
+window.addEventListener("DOMContentLoaded", (_) => {
+  // --------------- slideshow --------------------
+  let slideIndex = -1;
+  showSlides();
+  resize();
+
+  function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+
+    slides[slideIndex % slides.length].style.display = "block";
+
+    resize();
+    setTimeout(showSlides, 4000);
+  }
 });
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>French Recipes</title>
 
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./index.css">
 </head>
 <body>
 
@@ -19,26 +20,72 @@
         </ul>
     </nav>
 
-    <div class="container">
-        <div class="page-wrap">
-            <a href="aslkjfksdf">Recipe #1</a>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
-            width="300px" height="240px">
+    <div class="container-fluid recipe-home">
+        <div class="row">
+            <h1>Recipes of the Week</h1>
         </div>
-        <div class="page-wrap">
-            <a href="aslkjfksdf">Recipe #2</a>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
-            width="300px" height="240px">
+
+
+        <div class="row main-recipe">
+            <div class="slides">
+                <div class="col col-md-6">
+                    <h3>Baguette</h3>
+                    <div class="col col-md-6">
+                        <img class="main-recipe-img" src="https://www.196flavors.com/wp-content/uploads/2016/03/baguette-4-FP.jpeg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="slides">
+                <div class="col col-md-6">
+                    <h3>Croissant</h3>
+                </div>
+                <div class="col col-md-6">
+                    <img class="main-recipe-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU">
+                </div>
+            </div>
+
+            <div class="slides">
+                <div class="col col-md-6">
+                    <h3>Crepe</h3>
+                </div>
+                <div class="col col-md-6">
+                    <img class="main-recipe-img" src="https://joyfoodsunshine.com/wp-content/uploads/2022/03/best-crepes-recipe-1x1-1.jpg">
+                </div>
+            </div>
+
         </div>
-        <div class="page-wrap">
-            <a href="aslkjfksdf">Recipe #3</a>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
-            width="375px" height="300px">
+
+        <div class="row">
+            <h1>All Recipes</h1>
+        </div>
+
+
+
+
+        <div class="row">
+            <div class="page-wrap col col-md-4">
+                <a href="aslkjfksdf">Recipe #1</a>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
+                >
+            </div>
+            <div class="page-wrap col col-md-4">
+                <a href="aslkjfksdf">Recipe #2</a>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
+                >
+            </div>
+            <div class="page-wrap col col-md-4">
+                <a href="aslkjfksdf">Recipe #3</a>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
+                >
+            </div>
         </div>
     </div>
-    <?php
-    $color = "red";
-    echo '<h1>' . $color . '</h1>'; 
-    ?> 
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script src="./index.js"></script>
 </body>
 </html>
