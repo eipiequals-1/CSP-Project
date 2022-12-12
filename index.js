5aca0b9260eb656b6ffb352635eb62f9a585d618
eipiequals-1 <80728993+eipiequals-1@users.noreply.github.com>
2022-12-12 09:30:06 -0800

    added changes

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
   return { width: srcWidth * ratio, height: srcHeight * ratio };
 }
 
-function resize() {
-  const mainImages = document.getElementsByClassName("recipe-week-img");
-  for (let image of mainImages) {
-    const resize = calculateAspectRatioFit(
-      image.width,
-      image.height,
-      window.innerWidth / 2.0,
-      300
-    );
-    image.style.width = resize.width + "px";
-    image.style.height = resize.height + "px";
-  }
-}
-
 window.addEventListener("DOMContentLoaded", (_) => {
   const recipeWeek = document.getElementById("home");
   const recipeWeekName = document.getElementById("recipe-week-name");
@@ -25,23 +11,24 @@ window.addEventListener("DOMContentLoaded", (_) => {
   const recipeItems = [
     {
       name: "Baguette",
-      url: "https://www.196flavors.com/wp-content/uploads/2016/03/baguette-4-FP.jpeg"
+      url:
+        "https://www.196flavors.com/wp-content/uploads/2016/03/baguette-4-FP.jpeg",
     },
     {
       name: "Croissant",
-      url: "https://images.food52.com/fpQdXZahmggXW7ojSre7cNH6v_A=/1930x1286/filters:format(webp)/b2d9941f-3089-4fea-a3fc-4e9f80451fef--2021-0211_croissants_bake-it-up-a-notch_3x2_rocky-luten_034.jpg"
+      url:
+        "https://images.food52.com/fpQdXZahmggXW7ojSre7cNH6v_A=/1930x1286/filters:format(webp)/b2d9941f-3089-4fea-a3fc-4e9f80451fef--2021-0211_croissants_bake-it-up-a-notch_3x2_rocky-luten_034.jpg",
     },
     {
       name: "Crêpe",
-      url: "https://joyfoodsunshine.com/wp-content/uploads/2022/03/best-crepes-recipe-1x1-1.jpg"
-    }
+      url:
+        "https://joyfoodsunshine.com/wp-content/uploads/2022/03/best-crepes-recipe-1x1-1.jpg",
+    },
   ];
-
 
   // --------------- slideshow --------------------
   let slideIndex = -1;
   showSlides();
-  resize();
 
   function showSlides() {
     slideIndex++;
@@ -50,7 +37,6 @@ window.addEventListener("DOMContentLoaded", (_) => {
     recipeWeek.style.backgroundImage = `url("${recipe.url}")`;
     recipeWeekName.innerHTML = recipe.name;
 
-    resize();
     setTimeout(showSlides, 4 * 1000);
   }
 });
--- index.php
+++ index.php
@@ -1,91 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>French Recipes</title>
+    <title>Oh Mon Dieu | French Recipes</title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./index.css">
 </head>
+
 <body>
 
     <nav>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="news.asp">Recipes</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">About</a></li>
-            <input type="text">
+            <li><a href="#">Home</a></li>
+            <li><a href="#recipes">Recipes</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="about.html">About</a></li>
+            <div class="search-container">
+                <input type="text" placeholder="Search for recipes...">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
         </ul>
     </nav>
 
-    <div class="container-fluid recipe-home">
-        <div class="row">
-            <h1>Recipes of the Week</h1>
+    <section class="floating-section" id="home">
+        <h1>Recipes of the Week</h1>
+        <h2 id="recipe-week-name">Baguette</h2>
+        <button>Explore this recipe!</button>
+    </section>
+    <section class="floating-section" id="recipes">
+        <h1>Recipes</h1>
+        <div class="recipe-list">
+            <a href="aslkjfksdf">Recipe #1</a>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU">
         </div>
-
-
-        <div class="row main-recipe">
-            <div class="slides">
-                <div class="col col-md-6">
-                    <h3>Baguette</h3>
-                    <div class="col col-md-6">
-                        <img class="main-recipe-img" src="https://www.196flavors.com/wp-content/uploads/2016/03/baguette-4-FP.jpeg">
-                    </div>
-                </div>
-            </div>
-
-            <div class="slides">
-                <div class="col col-md-6">
-                    <h3>Croissant</h3>
-                </div>
-                <div class="col col-md-6">
-                    <img class="main-recipe-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU">
-                </div>
-            </div>
-
-            <div class="slides">
-                <div class="col col-md-6">
-                    <h3>Crepe</h3>
-                </div>
-                <div class="col col-md-6">
-                    <img class="main-recipe-img" src="https://joyfoodsunshine.com/wp-content/uploads/2022/03/best-crepes-recipe-1x1-1.jpg">
-                </div>
-            </div>
-
+        <div class="recipe-list">
+            <a href="aslkjfksdf">Recipe #2</a>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU">
         </div>
-
-        <div class="row">
-            <h1>All Recipes</h1>
+        <div class="recipe-list">
+            <a href="aslkjfksdf">Recipe #3</a>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU">
         </div>
+    </section>
 
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "usbw";
 
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
 
-
-        <div class="row">
-            <div class="page-wrap col col-md-4">
-                <a href="aslkjfksdf">Recipe #1</a>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
-                >
-            </div>
-            <div class="page-wrap col col-md-4">
-                <a href="aslkjfksdf">Recipe #2</a>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
-                >
-            </div>
-            <div class="page-wrap col col-md-4">
-                <a href="aslkjfksdf">Recipe #3</a>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFsK6RlRLoTW9NI8gz2Y-E5rhY_81vgD2lunObQ5q9yxQeVnHInLVLdYfKxvXW7JmYjGo&usqp=CAU"
-                >
-            </div>
-        </div>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "<h1>Connection failed</h1>";
+        }
+        echo "<h1>Connected successfully</h1>";
+        $conn->close();
+    ?>
 
     <script src="./index.js"></script>
 </body>
+
 </html>
