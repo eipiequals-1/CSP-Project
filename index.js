a8599fb5376569ea79ed9d443fc72367c15960b8
eipiequals-1 <olivier.lvannier@gmail.com>
2022-12-11 17:19:57 -0800

    added recipe of week as bg, and added flag as gb

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 }
 
 function resize() {
-  const mainImages = document.getElementsByClassName("main-recipe-img");
+  const mainImages = document.getElementsByClassName("recipe-week-img");
   for (let image of mainImages) {
     const resize = calculateAspectRatioFit(
       image.width,
@@ -19,21 +19,38 @@ function resize() {
 }
 
 window.addEventListener("DOMContentLoaded", (_) => {
+  const recipeWeek = document.getElementById("home");
+  const recipeWeekName = document.getElementById("recipe-week-name");
+
+  const recipeItems = [
+    {
+      name: "Baguette",
+      url: "https://www.196flavors.com/wp-content/uploads/2016/03/baguette-4-FP.jpeg"
+    },
+    {
+      name: "Croissant",
+      url: "https://images.food52.com/fpQdXZahmggXW7ojSre7cNH6v_A=/1930x1286/filters:format(webp)/b2d9941f-3089-4fea-a3fc-4e9f80451fef--2021-0211_croissants_bake-it-up-a-notch_3x2_rocky-luten_034.jpg"
+    },
+    {
+      name: "Crêpe",
+      url: "https://joyfoodsunshine.com/wp-content/uploads/2022/03/best-crepes-recipe-1x1-1.jpg"
+    }
+  ];
+
+
   // --------------- slideshow --------------------
   let slideIndex = -1;
   showSlides();
   resize();
 
   function showSlides() {
-    let slides = document.getElementsByClassName("slides");
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
     slideIndex++;
 
-    slides[slideIndex % slides.length].style.display = "block";
+    const recipe = recipeItems[slideIndex % recipeItems.length];
+    recipeWeek.style.backgroundImage = `url("${recipe.url}")`;
+    recipeWeekName.innerHTML = recipe.name;
 
     resize();
-    setTimeout(showSlides, 4000);
+    setTimeout(showSlides, 4 * 1000);
   }
 });
