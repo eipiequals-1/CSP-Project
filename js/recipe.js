0d7c8d2a8cadffe3517b7d7d29d64a9dad6ae1a9
eipiequals-1 <olivier.lvannier@gmail.com>
2022-12-14 17:38:20 -0800

    refactored js, styles for clean code and add dynamic recipe

---
+++ js/recipe.js
@@ -0,0 +1,47 @@
+// code
+
+
+window.addEventListener("DOMContentLoaded", (_) => {
+    // get the title and image header
+    const recipeTitle = document.getElementById("home");
+    const recipeTitleText = document.getElementById("recipe-week-name");
+    // get the ingredients and procedure container
+    const ingredientsContainer = document.getElementById("ingredients");
+    const procedureContainer = document.getElementById("procedure");
+
+    const recipe = recipeItems[1];
+    // change the document title
+    document.title = recipe.name + " Recipe";
+
+    // add title and background
+    recipeTitle.style.backgroundImage = `url("${recipe.url}")`;
+    recipeTitleText.innerHTML = recipe.name;
+
+    // add ingredients
+    let ingredients = "";
+    for (let ingredient of recipe.ingredients) {
+        ingredients += "<li>" + ingredient + "</li>";
+    }
+    ingredientsContainer.insertAdjacentHTML(
+        "beforeend",
+        `<h1>Ingredients</h1>
+        <ul>
+            ${ingredients}
+        </ul>
+        `
+    );
+
+    // add procedure
+    let procedure = "";
+    for (let p of recipe.procedure) {
+        procedure += "<li>" + p + "</li>";
+    }
+
+    procedureContainer.insertAdjacentHTML(
+        "beforeend",
+        `<h1>Procedure</h1>
+        <ol>
+            ${procedure}
+        </ol>`
+    );
+});
