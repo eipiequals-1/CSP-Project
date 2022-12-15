ca900f1114a8c96235e08b5d7be8013849ed6816
eipiequals-1 <80728993+eipiequals-1@users.noreply.github.com>
2022-12-15 10:06:50 -0800

    added stuff

--- index.php
+++ index.php
@@ -2,58 +2,39 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>French Recipes</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>French Recipes</title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./index.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="./styles/index.css">
 </head>
 
 <body>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#recipes">Recipes</a></li>
-        <li><a href="contact.html">Contact</a></li>
-        <li><a href="about.html">About</a></li>
-        <div class="search-container">
-          <input type="text" placeholder="Search for recipes..." />
-          <button type="submit"><i class="fa fa-search"></i></button>
-        </div>
-      </ul>
-    </nav>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#recipes">Recipes</a></li>
+      <li><a href="about.php">About</a></li>
+      <div class="search-container">
+        <input type="text" placeholder="Search for recipes..." />
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </div>
+    </ul>
+  </nav>
 
-    <section class="floating-section" id="home">
-      <h1>Recipes of the Week</h1>
-      <h2 id="recipe-week-name">Baguette</h2>
-      <a href="#recipes">Explore our recipes!</a>
-    </section>
-    <section class="floating-section" id="recipes">
-      <h1>Recipes</h1>
-      <p>Explore all of the delicious recipes!</p>
-    </section>
+  <section class="floating-section" id="home">
+    <h1>Recipes of the Week</h1>
+    <h2 id="recipe-week-name">Baguette</h2>
+    <a href="#recipes">Explore our recipes!</a>
+  </section>
+  <section class="floating-section" id="recipes">
+    <h1>Recipes</h1>
+    <p>Explore all of the delicious recipes!</p>
+  </section>
 
-    <script src="./index.js"></script>
-
-    <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "usbw";
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-            echo "<h1>Connection failed</h1>";
-        }
-        echo "<h1>Connected successfully</h1>";
-        $conn->close();
-    ?>
-
-    <script src="./index.js"></script>
+  <script src="./js/index.js"></script>
+  <script src="./js/recipes.js"></script>
 </body>
 
 </html>
--- recipe.php
+++ recipe.php
@@ -1,25 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sdljfhsdjlkf</title>
+    <title>French Recipes</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="./styles/recipe.css">
+
+    <style>
+        #ingredients ul,
+        #procedure {
+            text-align: left;
+            line-height: 2.5em;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Recipe page</h1>
     <?php
         $url = "http://" . $_SERVER["HTTP_HOST"] . $_SERVER['REQUEST_URI'];
         $recipe = htmlspecialchars($_GET["recipe"]);
-        
+        echo "<script>const recipeNameFromPhp = '" . $recipe . "';</script>";
+
         // database work
         $host = "localhost";
         $dbname = "p1recipe2022";
         $user = "root";
         $password = "";
         $port = 3306;
-        $db = new mysqli("localhost", $user, $password, $db) or die("Unable to connect to database!");
-        echo "great work";
+        // Create connection
+        $conn = new mysqli("localhost", $user, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "SELECT * FROM recipe";
+        $result = $conn->query($sql);
+
+        if ($result) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - Name: " . $row["first"]. " " . $row["last"]. "<br>";
+            }
+        }
     ?>
 
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Recipes</a></li>
+            <li><a href="about.php">About</a></li>
+            <div class="search-container">
+                <input type="text" placeholder="Search for recipes..." />
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+        </ul>
+    </nav>
+
+    <section class="floating-section" id="home">
+        <h1 id="recipe-week-name"></h1>
+    </section>
+    <section class="floating-section" id="ingredients"></section>
+    <section class="floating-section" id="procedure"></section>
+
+    <script src="./js/recipes.js"></script>
+    <script src="./js/recipe.js"></script>
 </body>
+
 </html>
